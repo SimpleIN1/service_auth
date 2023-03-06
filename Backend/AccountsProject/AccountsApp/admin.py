6546dbe2d2f8364a87bbee7f4d30e41eb235c541
user <user@mail.ru>
2023-03-07 00:53:47 +0700

    edited admin panel

--- Backend/AccountsProject/AccountsApp/admin.py
+++ Backend/AccountsProject/AccountsApp/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-from .models import User
+
+User = get_user_model()
 
 
 @admin.register(User)
-class UserModelAdmin(admin.ModelAdmin):
-    exclude = [
-        'uuid',
-        'is_reset_password',
-        'username',
-    ]
+class UserModelAdmin(UserAdmin):
+    # pass
+    # exclude = [
+    #     'uuid',
+    #     'is_reset_password',
+    #     'username',
+    # ]
     list_display = [
         'id',
         'email',
@@ -36,3 +41,47 @@ class UserModelAdmin(admin.ModelAdmin):
         'middle_name',
         'organization_name',
     ]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "last_name",
+                    "first_name",
+                    "middle_name",
+                    "organization_name",
+                    "password1",
+                    "password2"
+                ),
+            },
+        ),
+    )
+    fieldsets = (
+        (None, {"fields": ("password", )}), #"username",
+        (_("Personal info"), {
+            "fields":
+                (
+                    "last_name",
+                    "first_name",
+                    "middle_name",
+                    "organization_name",
+                    "email",
+                    "is_verify",
+                )
+        }),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
